3d66d59d8beda0d7f922b71da8d041266b4fed30
tartaman <70653340+tartaman@users.noreply.github.com>
2025-06-12 20:49:29 -0600

    revert

--- gestorpersonal/src/pages/Login.js
+++ gestorpersonal/src/pages/Login.js
@@ -17,7 +17,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://solicitudpersonal.saboria.me/api/login/', {
+      const response = await fetch('http://localhost:8000/api/login/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
